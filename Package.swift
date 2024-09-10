73779c2da9f3767d1578a1538eb38c7cda37343f
Fito Toledano <fito.toledano@gmail.com>
2024-09-10 12:27:39 +0200

    initial commit

--- Package.swift
+++ Package.swift
@@ -5,15 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "NotificationChip",
+    platforms: [.iOS(.v16), .macOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NotificationChip",
             targets: ["NotificationChip"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "NotificationChip"),
         .testTarget(
--- Sources/NotificationChip/NotificationChip.swift
+++ Sources/NotificationChip/NotificationChip.swift
@@ -1,2 +1,29 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+struct NotificationChip: View {
+    let title: String
+    let symbolName: String
+    
+    var body: some View {
+        HStack(spacing: 8) {
+            Image(systemName: symbolName)
+                .resizable()
+                .frame(width: 16, height: 16)
+            
+            Text(title)
+                .bold()
+        }
+        .background(.ultraThinMaterial)
+        .clipShape(RoundedRectangle(cornerRadius: 8))
+    }
+}
+
+#Preview {
+    NotificationChip(
+        title: "Added to playlist",
+        symbolName: "list.star"
+    )
+}
